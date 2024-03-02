78493db426b87ae1fd30a373d8ae3e352fea27bc
Ruth <57313608+ruth-cohen89@users.noreply.github.com>
2024-03-02 17:48:11 +0200

    Fixed getWorkout service

--- src/services/get-workout.js
+++ src/services/get-workout.js
@@ -1,13 +1,6 @@
 function makeGetWorkout({ workoutsDb }) {
   return async function getWorkoutUC({ id } = {}) {
-    let query = workoutsDb.getById(id);
-    query = query.select("-__v");
-
-    const workout = await query;
-    if (workout.length === 0) {
-      throw new NotFoundError("Workout can not be found");
-    }
-    return workout;
+    return await workoutsDb.getById(id);
   };
 }
 
