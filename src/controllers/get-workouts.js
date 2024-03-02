ce3598b4c6308b9f1612ddeca8b1609d26340c2b
Ruth <57313608+ruth-cohen89@users.noreply.github.com>
2024-03-02 17:27:27 +0200

    Fixed getWorkout controller

--- src/controllers/get-workouts.js
+++ src/controllers/get-workouts.js
@@ -1,7 +1,6 @@
 const getWorkouts = ({ getWorkoutsUC, catchAsync }) => {
   return catchAsync(async (req, res) => {
     const workouts = await getWorkoutsUC(req.body);
-    if (!workouts) return res.status(404).send({ error: "No Workouts exist" });
     res.status(200).send(workouts);
   });
 };
